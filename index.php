f583149d26dfce47d690bf82596648bc3e379dfe
AnderBarrantes <anderbarrantes30@gmail.com>
2020-12-24 18:54:17 -0500

    cambio menor

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
 <?php
-
 $empresas = ['empresas' => [
         ['usuario' => 'user1', 'password' => 'clave1'],
         ['usuario' => 'user2', 'password' => 'clave2'],
@@ -7,3 +13,6 @@ $empresas = ['empresas' => [
 ];
 
 echo json_encode($empresas);
+?>    
+</body>
+</html>
