fe67a0b682da90f8217b7b3b62ab22177867d729
AnderBarrantes <anderbarrantes30@gmail.com>
2020-12-24 18:37:27 -0500

    cambio menor

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-header('Content-Type: application/html');
-
 $empresas = ['empresas' => [
         ['usuario' => 'user1', 'password' => 'clave1'],
         ['usuario' => 'user2', 'password' => 'clave2'],
