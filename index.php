061393522f63d26a12df7dbf4938e33056ef1ba0
AnderBarrantes <anderbarrantes30@gmail.com>
2020-12-24 18:33:50 -0500

    cambio menor

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 header('Content-Type: application/html');
 
 $empresas = ['empresas' => [
-    ['usuario' => 'user1', 'password' => 'clave1'],
-    ['usuario' => 'user2', 'password' => 'clave2'], ],
+        ['usuario' => 'user1', 'password' => 'clave1'],
+        ['usuario' => 'user2', 'password' => 'clave2'],
+    ],
 ];
 
 echo json_encode($empresas);
