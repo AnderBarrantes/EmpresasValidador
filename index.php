886f602538050053facee192b6168de6b9858b98
AnderBarrantes <anderbarrantes30@gmail.com>
2020-12-24 19:02:45 -0500

    cambio menor

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD');
+header('Content-Type: application/json');
+
 $empresas = ['empresas' => [
         ['usuario' => 'user1', 'password' => 'clave1'],
         ['usuario' => 'user2', 'password' => 'clave2'],
     ],
 ];
+
 echo json_encode($empresas);
-?>    
-</body>
-</html>
